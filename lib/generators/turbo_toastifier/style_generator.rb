d986e817bbedca7d046b8d084a93c42992d4eba4
Thibault C. <thibault.chassine@gmail.com>
2026-04-17 11:14:11 +0200

    Add css

--- lib/generators/turbo_toastifier/style_generator.rb
+++ lib/generators/turbo_toastifier/style_generator.rb
@@ -4,10 +4,30 @@ module TurboToastifier
   class StyleGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
 
-    desc 'Generates a customizable SCSS file for TurboToastifier flash message styles'
+    class_option :format,
+                 type: :string,
+                 default: 'scss',
+                 enum: %w[scss css],
+                 desc: 'Stylesheet format to generate (scss or css)'
+
+    desc 'Generates customizable styles for TurboToastifier flash messages'
 
     def create_style_file
-      template 'turbo_toastifier.scss', 'app/assets/stylesheets/turbo_toastifier.scss'
+      template template_file_name, destination_file_name
+    end
+
+    private
+
+    def style_format
+      options.fetch(:format, 'scss')
+    end
+
+    def template_file_name
+      "turbo_toastifier.#{style_format}"
+    end
+
+    def destination_file_name
+      "app/assets/stylesheets/turbo_toastifier.#{style_format}"
     end
   end
 end
--- spec/generators/turbo_toastifier/style_generator_spec.rb
+++ spec/generators/turbo_toastifier/style_generator_spec.rb
@@ -7,7 +7,6 @@ require_relative '../../../lib/generators/turbo_toastifier/style_generator'
 
 RSpec.describe TurboToastifier::StyleGenerator do
   let(:destination_root) { File.expand_path('../../tmp/generator_test', __dir__) }
-  let(:generator) { TurboToastifier::StyleGenerator.new([], {}, { destination_root: destination_root }) }
 
   before do
     FileUtils.mkdir_p(destination_root)
@@ -19,46 +18,70 @@ RSpec.describe TurboToastifier::StyleGenerator do
   end
 
   describe 'generated files' do
-    before do
-      generator.create_style_file
+    context 'with default SCSS format' do
+      let(:generator) { TurboToastifier::StyleGenerator.new([], {}, { destination_root: destination_root }) }
+      let(:style_path) { File.join(destination_root, 'app', 'assets', 'stylesheets', 'turbo_toastifier.scss') }
+
+      before do
+        generator.create_style_file
+      end
+
+      it 'creates the style file' do
+        expect(File.exist?(style_path)).to be true
+      end
+
+      it 'includes nested flash message selectors' do
+        content = File.read(style_path)
+
+        expect(content).to include('&__message')
+        expect(content).to include('&-content')
+        expect(content).to include('&-close')
+      end
     end
 
-    let(:style_path) { File.join(destination_root, 'app', 'assets', 'stylesheets', 'turbo_toastifier.scss') }
+    context 'with CSS format' do
+      let(:generator) { TurboToastifier::StyleGenerator.new([], { format: 'css' }, { destination_root: destination_root }) }
+      let(:style_path) { File.join(destination_root, 'app', 'assets', 'stylesheets', 'turbo_toastifier.css') }
 
-    it 'creates the style file' do
-      expect(File.exist?(style_path)).to be true
-    end
+      before do
+        generator.create_style_file
+      end
 
-    it 'includes flash container styles' do
-      content = File.read(style_path)
-      expect(content).to include('.flash')
-      expect(content).to include('position: fixed')
-    end
+      it 'creates the css style file' do
+        expect(File.exist?(style_path)).to be true
+      end
 
-    it 'includes flash message styles' do
-      content = File.read(style_path)
-      expect(content).to include('&__message')
-      expect(content).to include('&-content')
-      expect(content).to include('&-close')
-    end
+      it 'includes flash container styles' do
+        content = File.read(style_path)
+        expect(content).to include('.flash')
+        expect(content).to include('position: fixed')
+      end
 
-    it 'includes flash type styles' do
-      content = File.read(style_path)
-      expect(content).to include('&__message.--notice')
-      expect(content).to include('&__message.--alert')
-      expect(content).to include('&__message.--warning')
-    end
+      it 'includes plain css flash message selectors' do
+        content = File.read(style_path)
+        expect(content).to include('.flash__message')
+        expect(content).to include('.flash__message-content')
+        expect(content).to include('.flash__message-close')
+      end
 
-    it 'includes animation keyframes' do
-      content = File.read(style_path)
-      expect(content).to include('@keyframes slide-in')
-      expect(content).to include('@keyframes fade-out-slide')
-    end
+      it 'includes flash type styles' do
+        content = File.read(style_path)
+        expect(content).to include('.flash__message.--notice')
+        expect(content).to include('.flash__message.--alert')
+        expect(content).to include('.flash__message.--warning')
+      end
 
-    it 'includes customization comments' do
-      content = File.read(style_path)
-      expect(content).to include('Customization Tips')
-      expect(content).to include('Run `rails generate turbo_toastifier:style`')
+      it 'includes animation keyframes' do
+        content = File.read(style_path)
+        expect(content).to include('@keyframes slide-in')
+        expect(content).to include('@keyframes fade-out-slide')
+      end
+
+      it 'includes tailwind-friendly import guidance' do
+        content = File.read(style_path)
+        expect(content).to include('Run `rails generate turbo_toastifier:style --format=css`')
+        expect(content).to include('@import "../stylesheets/turbo_toastifier.css";')
+      end
     end
   end
 end
